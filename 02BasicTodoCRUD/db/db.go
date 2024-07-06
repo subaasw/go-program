642b2e211d74deaef6b3bf1acc93e1faa9bff328
Subash Giri <mrsbs.giri@gmail.com>
2024-07-06 21:19:16 +0545

    feat: basic todo project added

---
+++ 02BasicTodoCRUD/db/db.go
@@ -0,0 +1,170 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+const TABLE_NAME = "Todo"
+
+type myDB struct {
+	conn *sql.DB
+}
+
+type Todo struct {
+	id                 int
+	title, description string
+}
+
+func (todo Todo) String() string {
+	return fmt.Sprintf("%d.\nTitle: %s,\nDescription: %s\n", todo.id, todo.title, todo.description)
+}
+
+func goDotEnvVariable(key string) string {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+// database connection
+func DBConn() *myDB {
+	dbDriver := "mysql"
+	dbUser := goDotEnvVariable("DBUSER")
+	dbName := goDotEnvVariable("DBNAME")
+	dbPass := goDotEnvVariable("DBPASS")
+
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return &myDB{conn: db}
+}
+
+func (db *myDB) Close() {
+	defer db.conn.Close()
+}
+
+// create table if not exists
+func (db *myDB) MaybeCreateTable() {
+
+	query := `
+		CREATE TABLE IF NOT EXISTS Todo (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			title TEXT NOT NULL,
+			description TEXT
+		);`
+
+	_, err := db.conn.Exec(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	println()
+	fmt.Println("| Table created Successfully |")
+	println()
+}
+
+// get data by id
+func (db *myDB) GetById(todoId int) int {
+	var todo Todo
+
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id = ?", TABLE_NAME)
+	if err := db.conn.QueryRow(query, todoId).Scan(&todo.id, &todo.title, &todo.description); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(todo.String())
+	return todo.id
+}
+
+// fetch all data
+func (db *myDB) GetAll() {
+	query := fmt.Sprintf("SELECT * FROM %s", TABLE_NAME)
+
+	rows, err1 := db.conn.Query(query)
+
+	if err1 != nil {
+		panic(err1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var todo Todo
+
+		err2 := rows.Scan(&todo.id, &todo.title, &todo.description)
+		if err2 != nil {
+			panic(err2)
+		}
+
+		fmt.Println(todo.String())
+	}
+
+	if err3 := rows.Err(); err3 != nil {
+		panic(err3)
+	}
+
+}
+
+// insert todo
+func (db *myDB) AddTodo(title string, description string) int64 {
+	query := fmt.Sprintf("INSERT INTO %s (title, description) VALUES (?, ?)", TABLE_NAME)
+
+	result, err := db.conn.Exec(query, title, description)
+
+	if err != nil {
+		panic(err)
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+
+	if rowsAffected > 0 {
+		fmt.Println(rowsAffected, "Added Successfully!")
+	}
+	return rowsAffected
+
+}
+
+func (db *myDB) UpdateTodo(id int, title string, description string) {
+	query := fmt.Sprintf("UPDATE %s SET title = ? , description = ? WHERE id = ?", TABLE_NAME)
+
+	result, err := db.conn.Exec(query, title, description, id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rA, _ := result.RowsAffected()
+
+	if rA > 0 {
+		fmt.Println("Updated Successfully")
+	} else {
+		fmt.Println("Something went wrong!")
+	}
+}
+
+// remove from table
+func (db *myDB) Remove(id int) {
+	query := fmt.Sprintf("DELETE from %s where id = ?", TABLE_NAME)
+	res, err := db.conn.Exec(query, id)
+
+	if err != nil {
+		fmt.Println("Something went wrong!")
+	}
+
+	if rF, _ := res.RowsAffected(); rF > 0 {
+		fmt.Println("Successfully Removed!")
+	}
+
+}
---
+++ 02BasicTodoCRUD/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"simple_crud/db"
+	"strings"
+)
+
+func printMsg(message string) {
+	fmt.Println(message)
+}
+
+func scanText() string {
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(line)
+}
+
+func main() {
+	var option int
+	todoDB := db.DBConn()
+
+	todoDB.MaybeCreateTable()
+
+	printMsg("# Welcome to Todo app")
+	println()
+
+	for {
+
+		printMsg("What you want to do ?")
+		printMsg(" 1. Add Todo")
+		printMsg(" 2. Get Todo by id")
+		printMsg(" 3. Retrive All Todos")
+		printMsg(" 4. Update Todo")
+		printMsg(" 5. Remove Todo")
+		printMsg(" 6. Exit")
+
+		fmt.Print("\nChoose: ")
+		fmt.Scanln(&option)
+
+		if option == 6 {
+			printMsg("\nBye bye 👋")
+			break
+		}
+
+		switch option {
+		case 1:
+			var title, description string
+
+			fmt.Print("Title Please: ")
+			title = scanText()
+
+			fmt.Print("Description Please: ")
+			description = scanText()
+
+			todoDB.AddTodo(title, description)
+		case 2:
+			var id int
+
+			fmt.Print("Enter Id: ")
+			fmt.Scanln(&id)
+
+			todoDB.GetById(id)
+		case 3:
+			printMsg("All Todos:")
+			todoDB.GetAll()
+		case 4:
+			var id int
+			var title, description string
+
+			fmt.Print("Todo Id Please: ")
+			fmt.Scanln(&id)
+
+			todoId := todoDB.GetById(id)
+
+			if todoId > 0 {
+				fmt.Print("Title Please: ")
+				title = scanText()
+
+				fmt.Print("Description Please: ")
+				description = scanText()
+
+				todoDB.UpdateTodo(id, title, description)
+			} else {
+				printMsg("Sorry Id not found")
+			}
+		case 5:
+			var id int
+			fmt.Print("Todo id to remove: ")
+			fmt.Scanln(&id)
+
+			todoDB.Remove(id)
+		default:
+			printMsg("Please select valid option")
+		}
+
+	}
+
+	todoDB.Close()
+}
